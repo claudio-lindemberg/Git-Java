db2228fd331ee2693149e3bff9de5199982b0fa0
Claudio Lindember <claudio.lindemberg@hotmail.com>
2022-03-24 23:23:16 -0300

    Idade foi adicionada

---
+++ ExerGit02.java
@@ -0,0 +1,32 @@
+package ExerGit;
+
+import java.util.Scanner;
+
+public class ExerGit01 {
+
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+		
+    System.out.println("Digite seu E-mail: ");
+    String email = scan.nextLine();
+		
+    System.out.println("Digite sua senha: ");
+    String senha = scan.nextLine();
+		
+    if (email+senha !=null) {
+      System.out.println("Logado com sucesso!");		
+    } else {
+      System.out.println("Credenciais incorretas!");
+    }		
+    
+    System.out.println("Digite sua idade: ");
+    int idade = scan.nextInt();
+    
+    if (idade>=18) {
+      System.out.println("Acesso concedido! maior de idade.");
+    } else {
+      System.out.println("Acesso restrito! menor de idade.");
+    }
+  }
+}
