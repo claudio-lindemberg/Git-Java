795c487ef4b6a6c1f7458941befa26a75a8b31b3
Cláudio Lindemberg <100038503+claudio-lindemberg@users.noreply.github.com>
2022-03-24 22:47:48 -0300

    identação do código

--- ExerGit01.java
+++ ExerGit01.java
@@ -4,25 +4,20 @@ import java.util.Scanner;
 
 public class ExerGit01 {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
 		
-		System.out.println("Digite seu E-mail: ");
-		String email = scan.nextLine();
+    System.out.println("Digite seu E-mail: ");
+    String email = scan.nextLine();
 		
-		System.out.println("Digite sua senha: ");
-		String senha = scan.nextLine();
+    System.out.println("Digite sua senha: ");
+    String senha = scan.nextLine();
 		
-		if (email+senha !=null) {
-			System.out.println("Logado com sucesso!");
-			
-		} else {
-			System.out.println("Credenciais incorretas!");
-		}
-		
-		
-		
-	}
-
+    if (email+senha !=null) {
+      System.out.println("Logado com sucesso!");		
+    } else {
+      System.out.println("Credenciais incorretas!");
+    }		
+  }
 }
